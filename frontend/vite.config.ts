bc2c906d8687bf64110769ba4091c4fbdacb7eae
bt66 <bastian.bt66@gmail.com>
2025-08-28 08:43:10 +0700

    fix env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { BACKEND_ENDPOINT } from '$env/static/private';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		proxy: {
 			'/api': {
-				target: 'https://e-guest-book-pjt-semarang-demak.vercel.app',
+				target: BACKEND_ENDPOINT,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
 			}
